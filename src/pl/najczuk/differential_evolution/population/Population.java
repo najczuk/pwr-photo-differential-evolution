c07380f99f54d70a65f1769435a616773ed88570
respectum <najczuk@gmail.com>
2014-12-31 16:46:34 +0100

    draft populacji

--- src/pl/najczuk/differential_evolution/population/Population.java
+++ src/pl/najczuk/differential_evolution/population/Population.java
@@ -1,9 +1,35 @@
 package pl.najczuk.differential_evolution.population;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
  * User: Adrian
  * Date: 12/31/2014
  * Time: 10:24
  */
 public class Population {
+    ArrayList<Genotype> genotypes;
+    private double fVector[];
+
+    public Population() {
+    }
+
+    public BufferedImage generateMutant(){
+        return  null; //TODO
+    }
+
+    private Genotype crossover(){
+        return  null; //TODO
+    }
+
+    private Genotype generateDifferentialGenotype(){
+        return null; //TODO
+    }
+
+    private Genotype generateAdditiveGenotype(Genotype differentialGenotype){
+        return null;
+    }
+
+
 }
